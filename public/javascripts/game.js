fbb483edd169dd503a752c9655ef1e04ede243ed
Fabien Nicollet <fnicollet@gmail.com>
2017-05-14 10:35:00 +0200

    new round logic

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -2,11 +2,16 @@ var app = angular.module('GameApp', []);
 
 app.controller('GameCtrl', function($scope, $http) {
     $scope.game = null;
+    $scope.gameInit = false;
     $scope.gameState = 0;
     $scope.reloadGameInterval = null;
     $scope.PING_INTERVAL = 100;
+    $scope.message = null;
+    $scope.initTimeout = null;
 
     $scope.onRoundStarted = function(){
+        $scope.gameInit = false;
+        $scope.message = "...Get Ready...";
         var message = "Get ready, bitches";
         $http.get('/game/say?message=' + encodeURIComponent(message)).then(function(response) {
         });
@@ -16,12 +21,17 @@ app.controller('GameCtrl', function($scope, $http) {
     };
 
     $scope.gameFinished = function(){
+        $scope.gameInit = false;
         $scope.gameState = 2;
     };
     $scope.reloadGame = function(){
         $http.get('/game/load?id=' + $scope.game.id).then(function(response) {
             $scope.game = response.data;
             if ($scope.game.hasOwnProperty("player1") || $scope.game.hasOwnProperty("player2")){
+                if ($scope.initTimeout != null){
+                    clearTimeout($scope.initTimeout);
+                    $scope.initTimeout = null;
+                }
                 $scope.gameFinished();
             }
         });
@@ -35,12 +45,13 @@ app.controller('GameCtrl', function($scope, $http) {
         $http.get('/game/start?id=' + $scope.gameId).then(function(response) {
             $scope.game = response.data;
             $scope.gameState = 1;
-            setTimeout(() => $http.get(`/game/init?id=${$scope.game.id}`), 3000 + Math.random() * 10000);
-            var message = "Get ready, bitches";
-            $http.get('/game/say?message=' + encodeURIComponent(message))
-                .then(function(response) {
-                });
             $scope.onRoundStarted();
+            var waitTime = window.location.href.indexOf("debug") != -1 ? 0 : 3000 + Math.random() * 10000;
+            $scope.initTimeout = setTimeout(() => {
+                $http.get(`/game/init?id=${$scope.game.id}`);
+                $scope.gameInit = true;
+                $scope.message = "FIRE!";
+            }, waitTime);
         });
     };
     $scope.nextRound = function(){
